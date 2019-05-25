d3c3a35f9b94d04deed1bee04246bb89c1a2af98
ozaki <fs65ugumori@yahoo.co.jp>
2019-05-26 00:25:50 +0900

    show toast

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
@@ -18,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void showToast(View view) {
         Log.d(LOG_TAG, "show toast");
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
     }
 
     public void countUp(View view) {
