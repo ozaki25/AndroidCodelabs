bbade400d1a75e66ee3a546f8ab115149050ebdc
ozaki <fs65ugumori@yahoo.co.jp>
2019-05-26 00:20:43 +0900

    add click event handler

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
@@ -13,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(LOG_TAG, "Hello World");
+    }
+
+    public void showToast(View view) {
+        Log.d(LOG_TAG, "show toast");
+    }
+
+    public void countUp(View view) {
+        Log.d(LOG_TAG, "count up");
+
     }
 }
