d66a23beee0d758088621d1bb28986a351b54838
ozaki <fs65ugumori@yahoo.co.jp>
2019-05-26 00:34:23 +0900

    create counter

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -5,16 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "Hello World");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(LOG_TAG, "Hello World");
+        mShowCount = findViewById(R.id.show_count);
     }
 
     public void showToast(View view) {
@@ -25,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void countUp(View view) {
         Log.d(LOG_TAG, "count up");
-
+        mCount++;
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
     }
 }
